d5705b502fcb64348329c98de498df5d7d2c6db3
ximenchuifeng <ximenchuifeng03@gmail.com>
2019-05-29 17:24:17 +0800

    ccc

---
+++ wms-webapi/src/main/java/com/yyorz/wmswebapi/WmsWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.yyorz.wmswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsWebapiApplication.class, args);
+    }
+
+}
